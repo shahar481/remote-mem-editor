bd4cce62be288b9e562e0315bca348c7eca03681
Shahar sela <sshaharse1@gmail.com>
2020-10-05 15:22:36 +0300

    Changed structure of project

--- commander.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by shahar on 28/09/2020.
-//
-
-#include "commander.h"
-
-int start_processing_commands(int sockfd) {
-    
-}
--- commander.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by shahar on 28/09/2020.
-//
-
-#ifndef MEM_EDITOR_COMMANDER_H
-#define MEM_EDITOR_COMMANDER_H
-
-#endif //MEM_EDITOR_COMMANDER_H
